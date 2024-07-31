75ac5e7501db6348896353d1a0c5e5a752029fb2
chrisvalere03gmail.com <nz-valere>
2024-07-31 08:44:12 +0100

    responsive page

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-$('#btn').on('click', function(){
-    $('.big').hide()
-    $('.msg').show()
-})
-$('#btn1').on('click', function(){
-    $('.msg').hide()
-    $('.big').show()
+$(document).ready(function(){
+    $(".form").on('submit', function(event){
+        const email = $("#Email").val()
+        console.log('hi');
+        if (!email) {
+            console.log('hello');
+            event.preventDefault();
+        }else{
+            event.preventDefault();
+        $('.big').hide()
+        $('.msg').show()
+        }
+    })
+    
+    $('#btn1').on('click', function(){
+        $('.msg').hide()
+        $('.big').show()
+    })
+    
+    // console.log(email);
 })
